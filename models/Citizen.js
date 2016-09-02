09fc662ee825fdb0280dc356ccb3437bdf6290fe
Piero Divasto <pdivasto@gmail.com>
2016-09-02 03:48:30 +0200

    Modified parameters in Citizen

--- models/Citizen.js
+++ models/Citizen.js
@@ -10,12 +10,11 @@ var Citizen = new keystone.List('Citizen');
 // noinspection JSLastCommaInObjectLiteral
 
 Citizen.add({
-	username: { type: Types.Name, required: true, index: true, initial: false },
-	firstName: { type: Types.Name, required: true, index: true, initial: false },
-	lastName: { type: Types.Name, required: true, index: true, initial: false },
+	username: { type: Types.Text, initial: true, required: true, index: true },
+	name: { type: Types.Name, initial: true, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
-	gender: { type: Types.Select, options: 'male, female, undefined' },
+	gender: { type: Types.Select, initial: true, options: 'male, female, undefined' },
 	about: { type: Types.Textarea },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
